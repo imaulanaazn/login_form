12ca0ab948285b0e6f1225b04eadce2e174ce4bb
Irham Maulana <i.maulana.azn@gmail.com>
2023-02-19 07:10:40 +0700

    init git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./Form.css";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-function App() {
+
+const App = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [role, setRole] = useState("");
+  const notify = () => toast.success("Coming soon");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!isValidEmail(email)) {
+      toast.warning("Invalid email address");
+      return;
+    }
+
+    if (!role) {
+      toast.warning("please fill the role");
+      return;
+    }
+
+    notify()
+    setEmail("");
+    setPassword("");
+    setRole("");
+  };
+
+  const isValidEmail = (email) => {
+    const emailRegex = /\S+@\S+\.\S+/;
+    return emailRegex.test(email);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+      <ToastContainer 
+      position="top-center"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="light"
+      />
+        <form className="form" onSubmit={handleSubmit}>
+          <h2 className="form-title">Login</h2>
+          <div className="form-group">
+            <input
+              type="email"
+              id="email"
+              value={email}
+              className={email ? "filled" : ""}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            <label htmlFor="email">Email</label>
+          </div>
+          <div className="form-group">
+            <div className="password-input">
+              <input
+                type="password"
+                id="password"
+                value={password}
+                className={password ? "filled" : ""}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+              <label htmlFor="password">Password</label>
+            </div>
+          </div>
+          <div className="form-group">
+            <select
+              className={role ? "filled" : ""}
+              id="role"
+              value={role}
+              onChange={(e) => setRole(e.target.value)}
+            >
+              <option value=""> </option>
+              <option value="Admin">Admin</option>
+              <option value="Distributor">Distributor</option>
+              <option value="SubDistributor">SubDistributor</option>
+              <option value="Dealer">Dealer</option>
+              <option value="SubDealer">SubDealer</option>
+              <option value="Customer">Customer</option>
+              <option value="Employee">Employee</option>
+            </select>
+            <label htmlFor="role">Select user type</label>
+          </div>
+          <button type="submit" className="submit-btn">
+            Login
+          </button>
+        </form>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
