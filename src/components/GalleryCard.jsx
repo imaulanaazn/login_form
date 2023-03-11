5e7cf34265aa69e42a76fbf9d9c359410cc462bd
Irham Maulana <i.maulana.azn@gmail.com>
2023-03-11 08:10:35 +0700

    change the card url

--- src/components/GalleryCard.jsx
+++ src/components/GalleryCard.jsx
@@ -5,7 +5,7 @@ export default function GalleryCard(props) {
   const {name,img,url} = props.data
   console.log(props)
   return (
-    <Link to={`http://localhost:3000${url}`}>
+    <Link to={url}>
       <div className="w-[15rem] h-[15rem] bg-slate-200 relative shadow-lg">
           <img src={`images/${img}`} alt="component img" className='w-full h-full object-cover' />
           <h1 className='absolute bottom-0 left-0 bg-teal-500 w-full text-center text-white py-2'>{name}</h1>
